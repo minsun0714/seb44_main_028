3e0118768a0ff73af8f2c4b05bf75da0da8f13d3
jasmine0714 <woooooooow22@gmail.com>
2023-07-02 18:58:44 +0900

    🚚 카카오 로그인 버튼을 login 폴더에 추가

--- client/src/pages/Login/style.ts
+++ client/src/pages/Login/style.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const KakaoLoginButton = styled.div`
-  background-image: url('/kakao_login_button.png');
+  background-image: url('/login/kakao_login_button.png');
   background-repeat: no-repeat;
   border-radius: 10px;
   height: 50px;
