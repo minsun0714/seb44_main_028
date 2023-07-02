3ac1a88019a8103e103006b180fe500f34bf1aee
jasmine0714 <woooooooow22@gmail.com>
2023-07-02 18:52:22 +0900

    💄 카카오 로그인 버튼 생성

---
+++ client/src/pages/Login/style.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const KakaoLoginButton = styled.div`
+  background-image: url('/kakao_login_button.png');
+  background-repeat: no-repeat;
+  border-radius: 10px;
+  height: 50px;
+  width: 350px;
+`;
